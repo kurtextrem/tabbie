08cb20f38f777921b020c98dc8086695fabb02b8
Jacob Groß <kurtextrem@gmail.com>
2020-12-21 23:46:30 +0100

    remove analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,49 +1,41 @@
-import React from 'react';
-import thunk from 'redux-thunk'
+import React from 'react'
 import TabGroupDetailsView from './containers/TabGroupDetailsView'
 import TabGroupListView from './components/TabGroupListView'
-import analytics from './middleware/analytics'
 import chromeStorage from './middleware/chromeStorage'
 import tabManager from './middleware/tabManager'
 import tabbieApp from './reducers'
-import { Provider } from 'react-redux';
-import { Route, Router, browserHistory } from 'react-router';
-import { applyMiddleware, createStore } from 'redux';
-import { render } from 'react-dom';
-import { screenView } from './actions';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { Route, Router, browserHistory } from 'react-router'
+import { applyMiddleware, createStore } from 'redux'
+import { render } from 'react-dom'
+import { screenView } from './actions'
 
 const appStyle = {
-  paddingLeft: '2em',
-  paddingRight: '2em',
-  paddingTop: '1em',
-  paddingBottom: '1em',
-  height: '40em',
-  width: '30em',
-  boxSizing: 'border-box',
-};
+	boxSizing: 'border-box',
+	height: '40em',
+	paddingBottom: '1em',
+	paddingLeft: '2em',
+	paddingRight: '2em',
+	paddingTop: '1em',
+	width: '30em',
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = createStore(
-    tabbieApp,
-    applyMiddleware(thunk, analytics, chromeStorage, tabManager),
-  );
-  render(
-  <Provider store={store}>
-  <div style={appStyle}>
-  <Router history={browserHistory}>
-  <Route
-  path="/details/:tabGroupKey"
-  component={TabGroupDetailsView}
-  onEnter={() => store.dispatch(screenView('TabGroupDetails'))}
+	const store = createStore(tabbieApp, applyMiddleware(thunk, chromeStorage, tabManager))
+	render(
+		<Provider store={store}>
+			<div style={appStyle}>
+				<Router history={browserHistory}>
+					<Route
+						path="/details/:tabGroupKey"
+						component={TabGroupDetailsView}
+						onEnter={() => store.dispatch(screenView('TabGroupDetails'))}
 					/>
-  <Route
-  path="*"
-  component={TabGroupListView}
-  onEnter={() => store.dispatch(screenView('TabGroupList'))}
-					/>
+					<Route path="*" component={TabGroupListView} onEnter={() => store.dispatch(screenView('TabGroupList'))} />
 				</Router>
 			</div>
 		</Provider>,
-		document.getElementById('container'),
-  );
-});
+		document.getElementById('container')
+	)
+})
